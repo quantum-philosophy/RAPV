51dd521e4afc3ddc08ea90d46397a85f2c32b69c
Ivan Ukhov <uvsoft@gmail.com>
2013-07-16 08:55:48 +0200

    Refactor the speed tests

--- src/+Test/+Assess/private/speed.m
+++ src/+Test/+Assess/private/speed.m
@@ -7,13 +7,8 @@ function speed(varargin)
   experimentCount = length(experiment.range);
   repeat = experiment.get('repeat', ones(1, experimentCount));
 
-  fprintf('%15s%15s%15s%15s%15s%15s%15s%15s\n', ...
-    experiment.shortName, 'Dimensions', 'Nodes', ...
-    'Chaos, s', ...
-    'Analytic, m', 'Speedup, x', ...
-    'Numeric, h', 'Speedup, x');
-
-  measurements = zeros(experimentCount, 3);
+  fprintf('%15s%15s%15s%15s%15s\n', experiment.shortName, ...
+    'Dimensions', 'Nodes', 'Chaos, s', 'Speedup, x');
 
   for i = 1:experimentCount
     parameter = experiment.range(i);
@@ -22,57 +17,20 @@ function speed(varargin)
 
     options = experiment.configure(parameter);
 
-    analytic = Temperature.Analytical.DynamicSteadyState(options);
-    % numeric = Temperature.Numerical.DynamicSteadyState(options);
     chaos = Temperature.Chaos.DynamicSteadyState(options);
 
     fprintf('%15d', chaos.process.dimensionCount);
     fprintf('%15d', chaos.chaos.nodeCount);
 
+    totalTime = 0;
     for j = 1:repeat(i)
-      tic;
-      analytic.compute(options.dynamicPower, options.steadyStateOptions);
-      measurements(i, 2) = measurements(i, 2) + toc * sampleCount;
-    end
-
-    % for j = 1:repeat(i)
-    %  tic;
-    %  numeric.compute(options.dynamicPower, options.steadyStateOptions);
-    %  measurements(i, 3) = measurements(i, 3) + toc * sampleCount;
-    % end
-
-    %
-    % NOTE: It should be the last as it was found to affect the others.
-    %
-    for j = 1:repeat(i)
-      tic;
+      time = tic;
       chaos.compute(options.dynamicPower, options.steadyStateOptions);
-      measurements(i, 1) = measurements(i, 1) + toc;
+      totalTime = totalTime + toc(time);
     end
 
-    measurements(i, :) = measurements(i, :) / repeat(i);
-
-    fprintf('%15.2f', measurements(i, 1));
-    fprintf('%15.2f', measurements(i, 2) / 60);
-    fprintf('%15.2e', measurements(i, 2) / measurements(i, 1));
-    fprintf('%15.2f', measurements(i, 3) / 60 / 60);
-    fprintf('%15.2e', measurements(i, 3) / measurements(i, 1));
+    fprintf('%15.2f', totalTime / repeat(i));
+    fprintf('%15.2f', sampleCount / chaos.chaos.nodeCount);
     fprintf('\n');
   end
-
-  figure;
-
-  line(experiment.range, measurements(:, 1), ...
-    'Color', Color.pick(1), 'Marker', 'o')
-  line(experiment.range, measurements(:, 2), ...
-    'Color', Color.pick(2), 'Marker', 'x')
-  line(experiment.range, measurements(:, 3), ...
-    'Color', Color.pick(3), 'Marker', 's')
-
-  set(gca, 'YScale', 'log');
-
-  Plot.title('Comparison of computational speed: %s', experiment.name);
-  Plot.label(experiment.name, 'log(Time, s)');
-  Plot.legend('Polynomial Chaos', ...
-    'Monte Carlo (Analytic)', 'Monte Carlo (Numeric)');
 end
