e5edca31e5a2b8fcb79ded318e4501e1a39faad6
Ivan Ukhov <ivan.ukhov@gmail.com>
2013-08-07 09:03:01 +0200

    Minor changes

--- src/+Plot/solution.m
+++ src/+Plot/solution.m
@@ -15,10 +15,9 @@ function [ MTTFexp, Pburn, output ] = solution(pc, output, varargin)
     'figure', false, 'layout', 'one', 'range', 'unbounded');
   Plot.vline(Utils.toCelsius(options.temperatureLimit), ...
     'Color', 'k', 'LineStyle', '--');
+  Plot.title('Maximal temperature');
   Plot.label('Temperature, C', 'Probability');
-  Plot.title('P(Tmax > %.2f C) = %.4f', ...
-    Utils.toCelsius(options.temperatureLimit), Pburn);
-  Plot.legend('Probability density', 'Temperature limit');
+  Plot.legend('Probability density', sprintf('P(burn) = %.2f%%', Pburn * 100));
 
   if options.has('name')
     prefix = [ options.name, ': ' ];
@@ -26,10 +25,12 @@ function [ MTTFexp, Pburn, output ] = solution(pc, output, varargin)
     prefix = '';
   end
 
-  Plot.name('%sE(MTTF) = %.2e, P(Tmax > %.2f C) = %.4f', ...
-    prefix, MTTFexp, Utils.toCelsius(options.temperatureLimit), Pburn);
+  Plot.name('%sE(MTTF) = %.2e, P(Tmax > %.2f C) = %.2f%%', prefix, ...
+    MTTFexp, Utils.toCelsius(options.temperatureLimit), Pburn * 100);
 
   Data.observe(output.MTTFdata, 'range', 'unbounded');
-  Plot.label('MTTF', 'Probability');
-  Plot.title('Probability density');
+  Plot.vline(MTTFexp, 'Color', 'k', 'LineStyle', '--');
+  Plot.title('Mean time to failure');
+  Plot.label('Time, s', 'Probability');
+  Plot.legend('Probability density', sprintf('E(MTTF) = %.2e', MTTFexp));
 end
