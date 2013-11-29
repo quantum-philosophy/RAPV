608cd198eb7aa6b9a1c38c5e581e264339d3d4fc
Ivan Ukhov <ivan.ukhov@gmail.com>
2013-11-29 16:17:48 +0100

    Updated Test/Assess/speed*

--- src/+Test/+Assess/private/speed.m
+++ src/+Test/+Assess/private/speed.m
@@ -7,27 +7,31 @@ function speed(varargin)
   experimentCount = length(experiment.range);
   repeat = experiment.get('repeat', ones(1, experimentCount));
 
-  fprintf('%20s%20s%20s%20s%20s%20s%20s\n', 'Processors', 'Thermal nodes', ...
-    'Variables', 'Quadrature nodes', 'Time steps', 'Chaos, s', 'Speedup, x');
+  fprintf('%20s%20s%20s%20s%20s%20s%20s\n', 'Processors', ...
+    'Thermal nodes', 'Random variables', 'Quadrature nodes', ...
+    'Time steps', 'Surrogate, s', 'Speedup, x');
 
   for i = 1:experimentCount
     options = experiment.configure(experiment.range(i));
 
-    chaos = Temperature.Chaos.DynamicSteadyState(options);
+    surrogate = SystemVariation(options);
+
+    thermalNodeCount = surrogate.temperature.nodeCount;
+    dimensionCount = surrogate.surrogate.inputCount;
+    quadratureNodeCount = surrogate.surrogate.nodeCount;
 
     fprintf('%20d%20d%20d%20d%20d', options.processorCount, ...
-      chaos.nodeCount, chaos.process.dimensionCount, ...
-      chaos.chaos.nodeCount, options.stepCount);
+      thermalNodeCount, dimensionCount, quadratureNodeCount, options.stepCount);
 
     totalTime = 0;
     for j = 1:repeat(i)
       time = tic;
-      chaos.compute(options.dynamicPower, options.steadyStateOptions);
+      surrogate.compute(options.dynamicPower);
       totalTime = totalTime + toc(time);
     end
 
     fprintf('%20.2f', totalTime / repeat(i));
-    fprintf('%20.2f', sampleCount / chaos.chaos.nodeCount);
+    fprintf('%20.2f', sampleCount / quadratureNodeCount);
     fprintf('\n');
   end
 end
