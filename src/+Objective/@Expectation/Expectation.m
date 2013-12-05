e5194b6201df8922040c281adb3a8a3ec691e69f
Ivan Ukhov <ivan.ukhov@gmail.com>
2013-12-05 14:15:10 +0100

    Switch to Algorithm 2 by default

--- src/+Objective/@Expectation/Expectation.m
+++ src/+Objective/@Expectation/Expectation.m
@@ -6,8 +6,12 @@ classdef Expectation < Objective.Base
   end
 
   methods (Access = 'protected')
-    function fitness = evaluate(~, data)
-      fitness = -mean(data(:, this.targetIndex));
+    function fitness = evaluate(this, data)
+      %
+      % TODO: It should be positive for the energy consumption and
+      % negative for the maximal temperature and lifetime.
+      %
+      fitness = mean(data(:, this.targetIndex), 1);
     end
   end
 end
