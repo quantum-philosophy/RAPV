9210caec1308faf90afb3b56ff8e7c9814420a7b
Ivan Ukhov <ivan.ukhov@gmail.com>
2014-02-27 16:22:33 +0100

    WIP: improving the accuracy test

--- src/+Test/+Assess/accuracy.m
+++ src/+Test/+Assess/accuracy.m
@@ -5,11 +5,16 @@ function accuracy
   comparisonOptions = Options('quantity', 'pdf', 'range', 'unbounded', ...
     'method', 'smooth', 'distanceMetric', 'KLD', 'errorMetric', 'RMSE');
 
-  orderSet = [ 1 2 3 4 5 ];
-  sampleCountSet = [ 1e2 1e3 1e4 ];
+  caseCount = 10;
 
+  orderSet = [ 1 2 3 4 5 ];
   orderCount = length(orderSet);
+
+  sampleCountSet = [ 1e2 1e3 1e4 ];
   sampleCount = length(sampleCountSet);
+
+  quantityNames = SystemVariation.Base.quantityNames;
+  quantityCount = length(quantityNames);
 
   %
   % Monte Carlo
@@ -18,67 +23,51 @@ function accuracy
     'surrogateOptions', Options('sampleCount', max(sampleCountSet)));
 
   mc = SystemVariation(options);
-  output = mc.compute(options.dynamicPower);
 
-  mc = struct;
-  mc.expectation = cell(sampleCount, 1);
-  mc.variance = cell(sampleCount, 1);
-  mc.data = cell(sampleCount, 1);
+  %
+  % Polynomial chaos
+  %
+  surrogates = cell(1, orderCount);
 
-  for i = 1:sampleCount
-    mc.data{i} = output.data(1:sampleCountSet(i), :);
-    mc.expectation{i} = mean(mc.data{i}, 1);
-    mc.variance{i} = var(mc.data{i}, [], 1);
+  options = Configure.problem('surrogate', 'PolynomialChaos');
+  for i = 1:orderCount
+    options.surrogateOptions.order = orderSet(i);
+    surrogates{i} = SystemVariation(options);
   end
+
+  %
+  % Error metrics
+  %
+  metricNames = { 'E', 'Var', 'f' };
+  metricCount = length(metricNames);
 
   error = struct;
   error.expectation = cell(orderCount, sampleCount);
   error.variance = cell(orderCount, sampleCount);
   error.data = cell(orderCount, sampleCount);
-
-  %
-  % Polynomial chaos
-  %
-  options = Configure.problem('surrogate', 'PolynomialChaos');
-
   for i = 1:orderCount
-    options.surrogateOptions.order = orderSet(i);
-
-    surrogate = SystemVariation(options);
-
-    fprintf('%s: evaluating order %d...\n', class(surrogate), orderSet(i));
-    time = tic;
-
-    output = surrogate.compute(options.dynamicPower);
-    stats = surrogate.analyze(output);
-    data = surrogate.sample(output, max(sampleCountSet));
-
-    fprintf('%s: done in %.2f seconds.\n', class(surrogate), toc(time));
-
     for j = 1:sampleCount
-      fprintf('%s: comparing with %d samples...\n', ...
-        class(surrogate), sampleCountSet(j));
-      time = tic;
-
-      error.expectation{i, j} = abs( ...
-        (mc.expectation{j} - stats.expectation) ./ mc.expectation{j});
-      error.variance{i, j} = abs( ...
-        (mc.variance{j} - stats.variance) ./ mc.variance{j});
-      error.data{i, j} = zeros(1, surrogate.quantityCount);
-      for k = 1:surrogate.quantityCount
-        error.data{i, j}(k) = Utils.compareDistributions( ...
-          mc.data{j}(:, k), data(:, k), comparisonOptions);
-      end
-
-      fprintf('%s: done in %.2f seconds.\n', class(surrogate), toc(time));
+      error.data{i, j} = zeros(1, quantityCount);
     end
   end
 
-  metricNames = { 'E', 'Var', 'P' };
-  metricCount = length(metricNames);
+  for i = 1:caseCount
+    mapping = randi(processorCount, 1, taskCount);
+    priority = rand(1, taskCount);
 
-  quantityNames = SystemVariation.Base.quantityNames;
-  quantityCount = length(quantityNames);
+    schedule = options.scheduler.compute(mapping, priority);
+    dynamicPower = options.power.compute(schedule);
+
+    evaluate(dynamicPower);
+  end
+
+  error.expectation = error.expectation / caseCount;
+  error.variance = error.variance / caseCount;
+  for i = 1:orderCount
+    for j = 1:sampleCount
+      error.data{i, j} = error.data{i, j} / caseCount;
+    end
+  end
 
   for k = 1:quantityCount
     fprintf('%s:\n', quantityNames{k});
@@ -111,5 +100,51 @@ function accuracy
     end
 
     fprintf('\n');
+  end
+
+  function evaluate(dynamicPower)
+    output = mc.compute(dynamicPower);
+
+    mcStats = struct;
+    mcStats.expectation = cell(sampleCount, 1);
+    mcStats.variance = cell(sampleCount, 1);
+    mcStats.data = cell(sampleCount, 1);
+
+    for i = 1:sampleCount
+      mcStats.data{i} = output.data(1:sampleCountSet(i), :);
+      mcStats.expectation{i} = mean(mcStats.data{i}, 1);
+      mcStats.variance{i} = var(mcStats.data{i}, [], 1);
+    end
+
+    for i = 1:orderCount
+      surrogate = surrogates{i};
+
+      fprintf('%s: evaluating order %d...\n', class(surrogate), orderSet(i));
+      time = tic;
+
+      output = surrogate.compute(dynamicPower);
+      stats = surrogate.analyze(output);
+      stats.data = surrogate.sample(output, max(sampleCountSet));
+
+      fprintf('%s: done in %.2f seconds.\n', class(surrogate), toc(time));
+
+      for j = 1:sampleCount
+        fprintf('%s: comparing with %d samples...\n', ...
+          class(surrogate), sampleCountSet(j));
+        time = tic;
+
+        error.expectation{i, j} = error.expectation{i, j} + ...
+          abs((mcStats.expectation{j} - stats.expectation) ./ mcStats.expectation{j});
+        error.variance{i, j} = error.variance{i, j} + ...
+          abs((mcStats.variance{j} - stats.variance) ./ mcStats.variance{j});
+        for k = 1:surrogate.quantityCount
+          error.data{i, j}(k) = error.data{i, j}(k) + ...
+            Utils.compareDistributions(mcStats.data{j}(:, k), ...
+              stats.data(:, k), comparisonOptions);
+        end
+
+        fprintf('%s: done in %.2f seconds.\n', class(surrogate), toc(time));
+      end
+    end
   end
 end
